87e640a4ab303792cb4252b0f861f5cba08d63eb
Sam Henry <henry51@comcast.net>
2019-10-31 14:16:33 -0600

    added client template // overview area complete

---
+++ clientTemplate/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    console.log("test");
+});
+
+// Mobile Menu Functionality
+var isOpen = false
+
+$(".menuIcon").click(function(){
+    console.log("CLICKED");
+    if(isOpen == false){
+        isOpen = true;
+
+        // Hamburger Icon Animation
+        TweenMax.to("#topHam", 0.2, {rotation: "45", y: "7px", background: "#282828"});
+        TweenMax.to("#midHam", 0.2, {opacity: "0"});
+        TweenMax.to("#bottomHam", 0.2, {rotation: "-45", y: "-7px", background: "#282828"});
+
+        // Menu Animation
+        TweenMax.to(".offPageMenu", 1, {height: "100vh"});
+        TweenMax.to("#menuList li:nth-child(1)", 0.3, {x: "0px", opacity: "1", delay: 0.5, ease: Power2.easeOut});
+        TweenMax.to("#menuList li:nth-child(2)", 0.3, {x: "0px", opacity: "1", delay: 0.6, eease: Power2.easeOut});
+        TweenMax.to("#menuList li:nth-child(3)", 0.3, {x: "0px", opacity: "1", delay: 0.7, ease: Power2.easeOut});
+        TweenMax.to("#menuList li:nth-child(4)", 0.3, {x: "0px", opacity: "1", delay: 0.8, ease: Power2.easeOut});
+        TweenMax.to("#menuList li:nth-child(5)", 0.3, {x: "0px", opacity: "1", delay: 0.9, ease: Power2.easeOut});
+    }else{
+        isOpen = false;
+
+        // Hamburger Icon Animation
+        TweenMax.to("#topHam", 0.2, {rotation: "0", y: "0px", background: "#ffffff"});
+        TweenMax.to("#midHam", 0.2, {opacity: "1"});
+        TweenMax.to("#bottomHam", 0.2, {rotation: "0", y: "0px", background: "#ffffff"});
+
+        // Menu Animation
+        TweenMax.to("#menuList li:nth-child(1)", 0.3, {x: "-100px", opacity: "0", delay: 0.4});
+        TweenMax.to("#menuList li:nth-child(2)", 0.3, {x: "-100px", opacity: "0", delay: 0.3});
+        TweenMax.to("#menuList li:nth-child(3)", 0.3, {x: "-100px", opacity: "0", delay: 0.2});
+        TweenMax.to("#menuList li:nth-child(4)", 0.3, {x: "-100px", opacity: "0", delay: 0.1});
+        TweenMax.to("#menuList li:nth-child(5)", 0.3, {x: "-100px", opacity: "0"});
+        TweenMax.to(".offPageMenu", 1, {height: "0vh", delay: 0});
+    }
+});
